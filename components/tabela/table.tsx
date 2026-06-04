4c022bd05ae59f4efdc4feb6c5d421edbed09e3d
Annette Carvalho <varelannette0@gmail.com>
2026-06-04 10:14:30 -0100

    correcao3

--- components/tabela/table.tsx
+++ components/tabela/table.tsx
@@ -47,9 +47,13 @@ export const RequestTable = () => {
             );
         }
     };
-        useEffect(() => {
-                fetchData();
-            }, []);
+       useEffect(() => {
+  const run = async () => {
+    await fetchData();
+  };
+
+  run();
+}, []);
     return (
         <div>
             <div className="max-w-6xl mx-auto w-full px-4 sm:px-6 lg:px-8 py-4">
